7be1fecb6ff01538e7fee7226c73ec6a1141dd3d
isaccanedo <isaccanedo@gmail.com>
2022-01-12 08:28:32 -0300

    Initial commit

---
+++ src/test/java/com/isaccanedo/jpa/JpaOneToManyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isaccanedo.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class JpaOneToManyDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
